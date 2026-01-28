6396e9919a137c6d0e918c39093359e49b779839
akilar.laio <akilar.liao@akilar.liao>
2026-01-28 15:00:22 +0800

    adjust.

---
+++ Assets/Demos/ReflectionWithURP/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_selfTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_selfTransform.Rotate(new Vector3(0.0f, Time.deltaTime * 30.0f, 0.0f));
+    }
+    private Transform m_selfTransform = null;
+}
